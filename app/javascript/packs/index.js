15b2d66f8a6361c709958e9ba1ac02fc8acdbd02
sumi-re01 <owob.viola@gmail.com>
2021-04-10 13:10:40 +0000

    [Add]header and message

---
+++ app/javascript/packs/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue/dist/vue.esm';
+import * as Bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap';
+
+import Header from './components/layouts/Header.vue';
+
+Vue.use(Bootstrap);
+
+const app = new Vue({
+    el: '.app',
+    components: {
+        'nav-bar' : Header
+    },
+    data: function() {
+        return {
+            message: "Hello World! Vue.js"
+        }
+    }
+})
